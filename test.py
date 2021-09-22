5b7598a2436400f6d37de3188c8d70c03822bf4c
IvanShipilow <shipilov_03@inbox.ru>
2021-09-22 16:34:04 +0300

    function

--- test.py
+++ test.py
@@ -1,8 +1,13 @@
-age = 23
+age = int(input('сколько тебе лет?'))
 if (age >= 23):
     print('тебе можно входить')
 elif (age >=18) and (age < 23):
     print('тебе можно с ограничиным временем')
 else:
     print('тебе сюда нельзя')
-      
+
+x = int(input('Введите 1 число: '))
+y = int(input('Введите 2 число: ')) 
+def sum(a,b):
+    return a + b
+print(sum(x,y))    
