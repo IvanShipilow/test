ac5fe402dbc30634681ee6d7502968970625d1e6
IvanShipilow <shipilov_03@inbox.ru>
2021-09-23 18:30:52 +0300

    цикл

--- test.py
+++ test.py
@@ -5,14 +5,24 @@ elif (age >=18) and (age < 23):
     print('тебе можно с ограничиным временем')
 else:
     print('тебе сюда нельзя')
-
+# функции 
 x = int(input('Введите 1 число: '))
 y = int(input('Введите 2 число: ')) 
 def sum(a,b):
     return a + b
 print(sum(x,y))  
 print('\n')  
-
+# циклы
 name = 'цикл фор!'
 for i in name:
     print(i)
+print('\n')
+for k in range(1,6):
+    print(name)
+
+print('\n')
+i=1
+while i<=10:
+    if i != 5:
+      print(i)
+    i+=1    
